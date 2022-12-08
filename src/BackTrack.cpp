bfcd218a5e90f612d2481e80c69049995ad9cc21
LIANG X <liangx0111@outlook.com>
2022-12-07 21:26:06 -0500

    2022/12/7

---
+++ src/BackTrack.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+//77. Combinations
+void backtrack_comb(int n, int k, std::size_t idx,
+    std::vector<std::vector<int>>& result, std::vector<int>& path) {
+  if (path.size() == k) {
+    result.push_back(path);
+    return;
+  }
+
+  for (std::size_t i = idx; i < n + 2 - (k - path.size()); ++i) {
+    path.push_back(i);
+    backtrack_comb(n, k, i + 1, result, path);
+    path.pop_back();
+  }
+}
+
+std::vector<std::vector<int>> combine(int n, int k) {
+  std::vector<std::vector<int>> result;
+  std::vector<int> path;
+  backtrack_comb(n, k, 1, result, path);
+  return result;
+}
+
+//17. Letter Combinations of a Phone Number
+void backtrack_17(const std::string& digits, const std::size_t& idx,
+    std::vector<std::string>& result, std::string& str, 
+    const std::string letter_map[]) {
+  if (idx == digits.size()) {
+    result.push_back(str);
+    return;
+  }
+  std::size_t d = digits[idx] - '0';  
+  std::string letters = letter_map[d];
+  for (std::size_t i = idx; i < letters.size(); ++i) {
+    str.push_back(letters[i]);
+    backtrack_17(digits, idx, result, str, letter_map);
+    str.pop_back();
+  }
+}
+
+std::vector<std::string> letterCombinations(std::string digits) {
+  std::vector<std::string> result;
+  std::string str;
+  if (digits.size() == 0) return result;
+
+  const std::string letter_map[10] = {
+    "",
+    "",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tuv",
+    "wxyz",
+  };
+  backtrack_17(digits, 0, result, str, letter_map);
+  return result;
+}
+
+//39. Combination Sum
+void backtrack_39(const std::vector<int>& candidates, int target, std::size_t idx, 
+    std::vector<std::vector<int>>& result, std::vector<int>& comb) {
+  if (target < 0) return;
+  if (target == 0) {
+    result.push_back(comb);
+    return;
+  }
+
+  for (std::size_t i = idx; i < candidates.size() && candidates[i] < target; ++i) {
+    comb.push_back(candidates[i]);
+    backtrack_39(candidates, target - candidates[i], i, result, comb);
+    comb.pop_back();
+  }
+}
+
+std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
+  std::vector<std::vector<int>> result;
+  std::vector<int> comb;
+  backtrack_39(candidates, target, 0, result, comb);
+  return result;
+}
